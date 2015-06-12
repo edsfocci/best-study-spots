d37c6bde836f6dd90b07003f7196ff40f804964d
Ed Solis <edsfocci@gmail.com>
2015-06-12 13:22:24 -0500

    Add SQLAlchemy skeleton

--- hacker_hangouts/__init__.py
+++ hacker_hangouts/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
+import hacker_hangouts.models
+
 import hacker_hangouts.views
---
+++ hacker_hangouts/models.py
@@ -0,0 +1,10 @@
+from hacker_hangouts import app
+from flask.ext.sqlalchemy import SQLAlchemy
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#db = SQLAlchemy(app)
+
+#class User(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.String(80), unique=True)
+#    email = db.Column(db.String(120), unique=True)
